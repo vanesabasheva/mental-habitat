c3ad7878da5350c40d0a3fec4da41ae984899f1a
Vanesa Basheva <e12119027@student.tuwien.ac.at>
2024-11-03 16:01:24 +0100

    add bcryptjs, add Vercel config

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const logger = require("../app");
 
 exports.postRegister = async (req, res) => {
